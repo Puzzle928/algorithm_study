a0b29807c9a67881c58c2504c2c9c5d99429c0f3
Kang Jeong Hyeon <83824814+Puzzle928@users.noreply.github.com>
2021-06-17 00:09:38 +0900

    4번까지했음

--- easy_algorithm.py
+++ easy_algorithm.py
@@ -2,15 +2,88 @@
 # 두 정수 a,b가 주어졌을 때 a와 b 사이에 속한 모든 정수의 합을 리턴하는 함수,solution을 완성하세요.
 # 예를 들어 a = 3. b = 5인 경우, 3+4+5 = 12 이므로 12를 리턴합니다.
 
-a = int(input())
-b = int(input())
-print(max(a,b))
-answer = 0
-for i in range(min(a,b),max(a,b)+1):
-    answer += i
+# a = int(input())
+# b = int(input())
+
+# answer = 0
+# for i in range(min(a,b),max(a,b)+1):
+#     answer += i
 
 
-print(answer)
+# print(answer)
+
+
+# 2. 수박수박수?
+# 길이가 n 수박수박수박을 반복하게끔하라
+# n 이 4 이면 수박수박
+# n 이 3 이면 수박수 리턴하게끔하라..
+
+
+# n = int(input())
+# watermellon = []
+# for i in range(1,n+1):
+#     if i % 2 != 0:
+#         watermellon.append("수")
+#     else:
+#         watermellon.append("박")
+
+# for e in watermellon:
+#     print(e, end ="")
+
+
+# 서울에서 김서방 찾기
+
+# String형 배열 seoul의 element중 "Kim"의 위치 x를 찾아, "김서방은 x에 있다"는 String을 반환하는 함수, solution을 완성하세요.
+# seoul에 "Kim"은 오직 한 번만 나타나며 잘못된 값이 입력되는 경우는 없습니다.
+
+# seoul = ["Jane", "Kim"]
+# for i,j in enumerate(seoul):
+#     if j == "Kim":
+#         print(j ,"의 위치는", i , "입니다")
+
+
+# 약수의 합
+
+# 정수 n을 입력받아 n의 약수를 모두 더한 값을 리턴하는 함수, solution을 완성해주세요.
+
+# n = int(input())
+# g = []
+
+# for i in range(1,n+1):
+#     if n % i == 0:
+#         g.append(i)
+#         g.append(n/i)
+
+# g = set(g)
+# print(sum(g))
+
+
+
+
+
+
+
+
+     
+    
+    
+    
+
+
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+        
 
 
 
