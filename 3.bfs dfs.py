851b9cad60c6491a062bf2e6b2c53870565aee38
Kang Jeong Hyeon <83824814+Puzzle928@users.noreply.github.com>
2021-06-27 00:12:10 +0900

    3주차 BFS 공부

--- 3.bfs dfs.py
+++ 3.bfs dfs.py
@@ -1,4 +1,4 @@
-# 깊이우선탐색의 구조
+# 깊이우선탐색의 구조(스택을 활용함)
 
 #             a
 #         b   c   d
@@ -8,8 +8,8 @@
 # a 검사하고 b 검사하고 e 검사하고  j검사하고 k 검사하고 f 검사하고 l 검사하고 c 검사하고 g 하고 정답을 찾고 종료한다
 
 # 스택을 활용하여 문제를 푼다
-
-# 예시문제
+#   
+# DFS 예시문제
 
 
 stack = [[0,0]]
@@ -28,18 +28,39 @@ while len(stack)>0:
             stack.append([y,x-1])
             maps[y][x-1] = 2
     if x + 1 < hori:
-        if maps[y][x+1] == 1:
+        if maps[y][x+1] == 0:
             stack.append([y,x+1])
             maps[y][x+1] = 2
     if y - 1 > -1:
-        if maps[y-1][x] == 1:
+        if maps[y-1][x] == 0:
             stack.append([y-1,x])
             maps[y-1][x] = 2
     if y + 1 < verti:
-        if maps[y+1][x] == 1:
+        if maps[y+1][x] == 0:
             stack.append([y+1,x])
             maps[y+1][x] = 2
-    print(False) 
+print(False) 
+
+# 너비우선탐색 BFS (큐 활용하기)
+def bfs():
+    queue = ??
+    answer = 0
+    while len(queue)> 0:
+        count = len(queue)
+        for time in range(count):
+            now = queue.pop(0)
+            if now == dest:
+                return answer
+            for i in data:
+                if i[0] == now and visited[i[1]-1] == False:
+                    queue.append(i[1])
+                    visited[i[1]-1] = True
+                elif i[1] == now and visited[i[0]-1] == False:
+                    queue.append(i[0])
+                    visited[i[0]-1] = True
+        answer += 1
+    return answer
+
 
 
 
